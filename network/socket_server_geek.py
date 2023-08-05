656511ab4d3a44918a1bd9bcd0d046959cdde81e
Horatio <alexanderchen1992@gmail.com>
2023-08-05 23:52:21 +0800

    add socket select

---
+++ network/socket_server_geek.py
@@ -0,0 +1,24 @@
+import socket
+
+
+def socket_server():
+    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                # if client is alive ,recv will wait, if client is close, recv will get ''
+                data = conn.recv(1024)
+                print(f"get data is {data}")
+                if not data:
+                    break
+                conn.sendall(data)
+
+
+if __name__ == '__main__':
+    socket_server()
