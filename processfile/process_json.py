e49303a27b7cb277ddcda0ae4f307255c42c814e
Horatio <alexanderchen1992@gmail.com>
2023-08-07 23:50:12 +0800

    play json dict and list

--- processfile/process_json.py
+++ processfile/process_json.py
@@ -3,7 +3,8 @@ import json
 
 def dump_json():
     print("dump str-----------------------")
-    content = {"name":"肯尼亚","level":"country","adcode":"肯尼亚","lng":37.86169142636884,"lat":0.534299750273016,"childrenNum":0,"parent":"null"}
+    content = {"name": "肯尼亚", "level": "country", "adcode": "肯尼亚", "lng": 37.86169142636884,
+               "lat": 0.534299750273016, "childrenNum": 0, "parent": "null"}
     res = json.dumps(obj=content)
     print(type(res))
     print(res)
@@ -11,6 +12,14 @@ def dump_json():
     print("dump file-----------------------")
     with open(file="country.json", mode="w") as f:
         json.dump(obj=content, fp=f, ensure_ascii=False)
+
+
+def dump_json_list():
+    ll = {"version": "xxx", "tree": [{"version": "aaa", "tree": [{"version": "yyy"}]}, {"version": "bbb"}]}
+    res = json.dumps(obj=ll)
+    print(res)
+    print(ll.get("tree").index({"version": "bbb"}))
+    print(ll.get("version"))
 
 
 def read_json():
@@ -30,5 +39,7 @@ def read_json():
 
 
 if __name__ == '__main__':
-    dump_json()
-    read_json()
+    # dump_json()
+    # read_json()
+
+    dump_json_list()
