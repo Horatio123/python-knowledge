b3da6fb2925fe16ea67256bc9798c9f1b9baf7bc
Horatio <alexanderchen1992@gmail.com>
2024-02-03 14:55:20 +0800

    use dataframe

--- compute/panda_learn.py
+++ compute/panda_learn.py
@@ -1,6 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 
+
 def play_with_pandas():
     df = pandas.read_csv('cwe.csv')
     print(type(df))
@@ -14,49 +15,44 @@ def analyse_population():
 
     print(type(df))
     # print(df['1977'])
-    china_df = df[df['Country Name'] == 'China'].iloc[:, 4:]
-    print(china_df)
+    big_df = df[(df['Country Name'] == 'China') | (df['Country Name'].str.contains('India'))].iloc[:, [0] + list(range(4, df.shape[1]))]
 
-    india_df = df[df['Country Name'] == 'India'].iloc[:, 4:]
-    print(india_df)
+    name_pop_df = big_df['Country Name'].apply(lambda x: x + '_population')
+    big_df.insert(1, 'country_pop', name_pop_df)
+    big_df = big_df.drop('Country Name', axis=1)
 
-    years = china_df.columns.tolist()  # 行标题作为横坐标
-    values = china_df.values.tolist()[0]  # 行内容作为纵坐标
-    print(years)
-    print(values)
-
-    # plt.bar(years, values[0])
-    # plt.xlabel('year')
-    # plt.ylabel('population')
-    # plt.title('China population')
-    # plt.xticks(rotation=45)  # 可选：旋转横坐标标签以更好地显示
-    # plt.show()
-
-    c_pop_df = pandas.DataFrame({
-        'years': years,
-        'population': values
-    })
-
-    i_pop_df = pandas.DataFrame({
-        'years': india_df.columns.tolist(),
-        'population': india_df.values.tolist()[0]
-    })
+    #
+    # years = big_df.columns.tolist()  # 行标题作为横坐标
+    # values = big_df.values.tolist()[0]  # 行内容作为纵坐标
+    # print(years)
+    # print(values)
 
     pop_df = pandas.DataFrame({
-        'years': india_df.columns.tolist(),
-        'china_population': china_df.values.tolist()[0],
-        'india_population': india_df.values.tolist()[0]
+        'years': big_df.columns.tolist()[1:],
+        big_df.values.tolist()[0][0]: big_df.values.tolist()[0][1:],
+        big_df.values.tolist()[1][0]: big_df.values.tolist()[1][1:]
     })
-
-    # i_pop_df.plot(kind='line', x='years', y='population')
-    # c_pop_df.plot(kind='line', x='years', y='population')
-    # plt.show()
 
     # 设置 'date' 列为索引，并确保它是日期类型
     pop_df.set_index('years', inplace=True)
 
     # 绘制多条线图
-    pop_df.plot.line(y=['china_population', 'india_population'], figsize=(10, 6))
+    pop_df.plot.line(y=[big_df.values.tolist()[0][0], big_df.values.tolist()[1][0]], figsize=(10, 6))
+
+    # 显示图形
+    plt.show()
+
+    gap_list = list(map(lambda x, y: x - y, big_df.values.tolist()[0][1:], big_df.values.tolist()[1][1:]))
+    gap_df = pandas.DataFrame({
+        'years': big_df.columns.tolist()[1:],
+        'gap_population': gap_list
+    })
+
+    # 设置 'date' 列为索引，并确保它是日期类型
+    gap_df.set_index('years', inplace=True)
+
+    # 绘制多条线图
+    gap_df.plot.line(y=['gap_population'], figsize=(10, 6))
 
     # 显示图形
     plt.show()
